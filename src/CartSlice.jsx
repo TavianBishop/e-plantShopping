cb22ac45bd7983f191f9f4f2fe8e48d54a8c123d
TavianBishop <BishopCoding@gmail.com>
2026-08-10 15:05:13 -0400

    add and remove item reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -9,9 +9,17 @@ export const CartSlice = createSlice({
   
   reducers: {
     addItem: (state, action) => {
-    
+    const {name,image,cost} = action.payload;
+
+    const exisitingItem = state.items.find(item => item.name === name);
+    if(exisitingItem){
+        exisitingItem.quantity ++;
+    } else{
+        state.items.push({name,image,cost,quantiy:1})
+    }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item =>  item !== action.payload);
     },
     updateQuantity: (state, action) => {
 
